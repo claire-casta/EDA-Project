f73a77f8f0c5a061422186258e900db95934237a
Claire Castanheira <clairecasta@gmail.com>
2024-06-20 11:29:33 +0100

    setup

---
+++ db_utils.py
@@ -0,0 +1,5 @@
+#will contain code to extract the data from the database
+import YAML
+class RDSDatabaseConnector:
+    # Will contain methods used to extract data from the database
+    __init__(credentials) #?????
