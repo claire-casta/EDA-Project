6829036fd45d474feaa6beec4d5f588449a14d46
Claire <clairecasta@gmail.com>
2024-07-13 18:37:24 +0100

    added missing docstrings

--- db_utils_local.py
+++ db_utils_local.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 class DataTransform:
+    """Various methods that transform the data within the dataframe."""
     def __init__(self, df):
         self.df = df
 
@@ -28,11 +29,12 @@ class DataTransform:
         return self.df
 
 class DataframeInfo:
+    """Various methods that give information about the dataframe."""
     def __init__(self, df):
         self.df = df
 
     def describe_columns(self):
-         """Describe all columns in the DataFrame to check their data types"""
+         """Describe all columns in the DataFrame to check their data types."""
          return self.df.dtypes
     
     def extract_statistics(self):
@@ -40,17 +42,17 @@ class DataframeInfo:
         return self.df.describe(include=None)
     
     def count_dist_values_cat(self):
-        """Count distinct values in categorical columns"""
+        """Count distinct values in categorical columns."""
         cat_cols = self.df.select_dtypes(include=['object', 'category'])            # Select only the categorical columns
         distinct_values = cat_cols.nunique()                                                      # Count the distinct values in each categorical column
         return distinct_values
     
     def shape_of_df(self):
-        """Print out the shape of the DataFrame"""
+        """Print out the shape of the DataFrame."""
         return self.df.shape
 
     def count_percent_null(self):
-        """Generate a count/percentage count of NULL values in each column"""
+        """Generate a count/percentage count of NULL values in each column."""
         count_null = self.df.isnull().sum()                                         #counts the nulls in columns
         percent_null = self.df.isnull().sum() * 100 / len(self.df)                  #% of nulls in columns
         null_values = pd.DataFrame({                                                  #creates a small DataFrame to show the data for each column
@@ -60,12 +62,14 @@ class DataframeInfo:
         return null_values
     
     def analyze_skew(self):
-        """Returns the skewness of each column"""
+        """Returns the skewness of each column."""
         numeric_columns = self.df.select_dtypes(include=['number']).columns
         skewness = self.df[numeric_columns].skew()
         return skewness
     
     def analyze_skew_methods(self, columns):
+        """Analyses the skewness of each column, to return a table of data cotaining column name, the original skew, box cox transformation and Yeo 
+        Johnson tnsformation"""
         import pandas as pd
         import numpy as np
         from scipy.stats import boxcox, skew, yeojohnson
@@ -107,85 +111,98 @@ class DataframeInfo:
         return pd.DataFrame(results)
 
     def correlation_columns(self):
+        """Returns the correlation between 2 columns to allow production of a correlation heatmap"""
         return self.df.select_dtypes(include=['number']).corr()
 
 
 class DataFrameTransform:
+     """Transforms the data using various methods"""                               
      def __init__(self, df):
         self.df = df
 
      def delete_column(self, column_name):
+         """Deletes the named column from the dataframe"""
          drop_df = self.df.drop(column_name, axis=1, inplace=True) # Drops named column
          return drop_df
      
      def delete_row_using_id(self, id_no):
+        """Deletes the named row from the dataframe, by matching the value in the id_no column"""
         self.df.drop(self.df[self.df['id'] == id_no].index, inplace=True)
         return self.df
      
      def delete_row(self, column_name):
+         """Deletes rows with null values in the specified column from the dataframe"""
          drop_df = self.df.dropna(subset=[column_name], inplace=True) # Drops rows with null values in named column
          return drop_df
      
      def delete_row_if_both_null(self, column_name_1, column_name_2):
+         """Deletes rows with null values in both the specified columns from the dataframe"""
          drop_df = self.df.dropna(subset=[column_name_1, column_name_2], how='all', inplace=True) # Drops rows with null values in both named columns
          return drop_df
      
      def impute_median(self, column_name):
+         """Replaces null value with median value"""
          median_impute = self.df[column_name].fillna(self.df[column_name].median(), inplace=True) # Replaces null value with median value
          return median_impute
      
      def impute_mode(self, column_name):
+         """Replaces null value with mode value"""
          mode_impute = self.df[column_name].fillna(self.df[column_name].mode()[0], inplace=True) # Replaces null value with mode value
          return mode_impute
      
      def impute_mean_by_category(self, Category_Column, Value_Column):
+         """Replaces null value with median value"""
          mean_per_category = self.df.groupby(Category_Column)[Value_Column].transform('mean')  #Calculate mean values per category
          ValueColumn_filled = self.df[Value_Column].fillna(mean_per_category, inplace=True)
          return ValueColumn_filled
      
      def impute_previous_row_value(self, Category_Column, Value_Column):
-        # Get unique categories
-        categories = self.df[Category_Column].unique()
-    
-        # Loop through each category
-        for category in categories:
+         """Imputes the value from the previous row into null values in the named value_column, when filtered by category within the category_column"""
+         categories = self.df[Category_Column].unique()
+         # Loop through each category
+         for category in categories:
             # Filter the DataFrame for the current category
             mask = self.df[Category_Column] == category
             # Back fill missing values within the category
             self.df.loc[mask, Value_Column] = self.df.loc[mask, Value_Column].bfill()
-        
-        return self.df
+         return self.df
      
      def impute_term(self, value_column, loan_amount, int_rate, instalment):
+         """Imputes a loan term into the value_columm (which in this project is loan_term) using a calculation that takes values from the loan amount, interest 
+         rate and installment amount columns.  As a by product of this process the annual interest rate is converted from a percentage to a decimal."""
            # Calculate loan term in months using the formula
-        import math
-        self.df[loan_amount] = pd.to_numeric(self.df[loan_amount])
-        self.df[int_rate] = pd.to_numeric(self.df[int_rate]) / 100  # Convert annual interest rate to decimal
-        self.df[instalment] = pd.to_numeric(self.df[instalment])
-        import numpy as np
-        def calc_loan_term_months(row):            
-            if str(row[value_column]) == "<NA>":
-                try:
-                    row[value_column] = round(-(math.log(1 - ((row[loan_amount] * ((row[int_rate]) / 12)) / row[instalment]))) / (math.log(1 + ((row[int_rate]) / 12))))
-                except Exception as e:
-                    print(row["id"])
-                    print(e)
-                    print(row[loan_amount])
-                    print(row[int_rate])
-                    print(row[instalment])
-                return row
-            else:
-                return row
-        self.df = self.df.apply(calc_loan_term_months, axis=1)
-        return self.df
+         import math
+         self.df[loan_amount] = pd.to_numeric(self.df[loan_amount])
+         self.df[int_rate] = pd.to_numeric(self.df[int_rate]) / 100  # Convert annual interest rate to decimal
+         self.df[instalment] = pd.to_numeric(self.df[instalment])
+         import numpy as np
+    
+     #def calc_loan_term_months(row):            
+         #"""Failed code that did not work, but kept for now"""
+         #if str(row[value_column]) == "<NA>":
+             #try:
+                 #row[value_column] = round(-(math.log(1 - ((row[loan_amount] * ((row[int_rate]) / 12)) / row[instalment]))) / (math.log(1 + ((row[int_rate]) / 12))))
+             #except Exception as e:
+                # print(row["id"])
+                # print(e)
+                 #print(row[loan_amount])
+                # print(row[int_rate])
+                 #print(row[instalment])
+                 #return row
+             #else:
+                 #return row
+         #self.df = self.df.apply(calc_loan_term_months, axis=1)
+         #return self.df
      
      def apply_box_cox(self, column_name):
+         """Transforms the data using the box-cox method"""                               
          from scipy.stats import boxcox
          transformed_data, _ = boxcox(self.df[column_name])
          self.df[column_name] = transformed_data
          return self.df[column_name]
      
      def apply_yeo_johnson(self, column_name):
+        """Transforms the data using the Yeo Johnson method"""                               
         from scipy.stats import yeojohnson
         transformed_data, _ = yeojohnson(self.df[column_name])
         self.df[column_name] = transformed_data
@@ -196,6 +213,7 @@ class DataFrameTransform:
          return self.df
     
      def remove_outliers_IQR(self, column_name):
+        """Removes outliers below the 25th centile and above the 75th centile for a named column, using the interquartile range."""
         # Calculate Q1 (25th percentile) and Q3 (75th percentile)
         Q1 = self.df[column_name].quantile(0.25)
         Q3 = self.df[column_name].quantile(0.75)
@@ -220,6 +238,7 @@ class Plotter:
         self.df = df
 
      def discrete_probability_distribution(self, column_name):
+         """Produces a discrete probability distribution for the named column."""
          import seaborn as sns
          import matplotlib.pyplot as plt
          plt.rc("axes.spines", top=False, right=False)
@@ -236,7 +255,7 @@ class Plotter:
          plt.show()
 
      def histogram_df_columns(self):
-
+        """Produces a histogram for all numeric columns within the dataframe."""
         import numpy as np
         import seaborn as sns
         import matplotlib.pyplot as plt
@@ -261,6 +280,7 @@ class Plotter:
         plt.show()
 
      def qqplot_df_columns(self):
+        """Produces a QQplot for all numeric columns within the dataframe."""
         import numpy as np
         import matplotlib.pyplot as plt
         from scipy import stats
@@ -283,6 +303,7 @@ class Plotter:
         plt.show()
 
      def boxplot_with_scatter(self):
+        """Produces a boxplot with scattergraph for all numeric columns within the dataframe."""
         import numpy as np
         import seaborn as sns
         import matplotlib.pyplot as plt
@@ -301,6 +322,7 @@ class Plotter:
         plt.show()
 
      def heatmap(self, matrix):
+        """Produces a heatmap based on the matrix given."""
         import seaborn as sns
         import matplotlib.pyplot as plt
 
